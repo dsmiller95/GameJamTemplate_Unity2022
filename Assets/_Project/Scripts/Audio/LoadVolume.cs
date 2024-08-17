9e4f2eb58b672ce94cfa6cd40ba6951cc78b3887
LimeBlossom <limeblossomstudio@gmail.com>
2024-08-17 04:12:34 +0100

    Quit game button functions

---
+++ Assets/_Project/Scripts/Audio/LoadVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class LoadVolume : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private string paramName;
+    [SerializeField] private Slider slider;    
+
+    void Start()
+    {
+        float volume = PlayerPrefs.GetFloat(mixer.name + paramName, 0.5f);
+        if(slider)
+        {
+            slider.value = volume;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Audio/SetVolume.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private string paramName;
+
+    public void SetLevel (float value)
+    {
+        mixer.SetFloat(paramName, Mathf.Log10(value) * 20);
+        PlayerPrefs.SetFloat(mixer.name + paramName, value);
+    }
+}
